a5eda684e5e8cd0c0a9bd518429be1a032c4699b
Marijoy Jatico <142898185+maripandari@users.noreply.github.com>
2023-09-14 14:45:59 -1000

    Completed Problem Sets

--- HackwichThree/ViewController.swift
+++ HackwichThree/ViewController.swift
@@ -12,9 +12,11 @@ class ViewController: UIViewController {
     var firstString = "The background will turn blue"
     var secondString = "The background color will turn green"
     var thirdString = "cool"
+    let number1 = 10
+    let number2 = 5
     
     
-  //Part 3
+    //Part 3
     @IBAction func changeColorButtonPressed(_ sender: Any) {
         
         if firstString == "The background color will turn red"
@@ -30,12 +32,33 @@ class ViewController: UIViewController {
     }
     
     @IBOutlet weak var firstLabel: UILabel!
+    @IBOutlet weak var secondLabel: UILabel!
+    @IBOutlet weak var thirdLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func textChangeColorButtonPressed(_ sender: Any) {
+        
+        if firstString == "The background will turn blue"
+        { secondLabel.text = "My Conditional Statement evaluates to true"}
+        
+        else
+        {thirdLabel.text = "My Conditional Statement evaluates to False" }
+        
+    }
+    
+    @IBAction func additionButtonPressed(_ sender: Any) {
+        
+        if firstString == "The background color will turn blue" { print("The code in this block will not execute")
+        }
+        else {  view.backgroundColor = UIColor.green
+            let sum = number1 + number2
+            print("The sum of \(number1) and \(number2) is \(sum)") }
+        
+    }
+    
 }
 
